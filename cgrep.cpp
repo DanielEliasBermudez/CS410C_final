0ac561bf9e68f7e24604aa36b76fd05e9e818845
Daniel Elias Bermudez <de.bermudez13@gmail.com>
2019-02-02 21:13:34 -0800

    Add arg parsing

--- cgrep.cpp
+++ cgrep.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <string>
+#include <sstream>
 using namespace std;
 
 void displayHelpMessage()
@@ -15,10 +16,62 @@ void displayHelpMessage()
          << "\tshow <NUM> lines after each match\n"
          << "-B <NUM>\n"
          << "\tshow <NUM> lines before each match\n"
+         << "-h\n"
+         << "\tdisplay this help message"
          << endl;
+    exit(0);
 }
 
 int main(int argc, char **argv)
 {
-    displayHelpMessage();
+    int numberOfLinesAfter;
+    int numberOfLinesBefore;
+
+    bool doneParsingOptions = false;
+    int patternIndex = 1;
+
+    for (int i = 1; i < argc; ++i)
+    {
+        if (!doneParsingOptions)
+        {
+            string option(argv[i]);
+            if (!strncmp(&option[0], "-", 1))
+            {
+                if (option == "-A")
+                {
+                    stringstream ss(argv[++i]);
+                    ss >> numberOfLinesAfter;
+                }
+                else if (option == "-B")
+                {
+                    stringstream ss(argv[++i]);
+                    ss >> numberOfLinesBefore;
+                }
+                else
+                {
+                    displayHelpMessage();
+                }
+            }
+            else
+            {
+                doneParsingOptions = true;
+                patternIndex = i;
+            }
+        }
+    }
+
+    if ((argc - patternIndex) == 2)
+    {
+        string pattern(argv[patternIndex]);
+        string path(argv[patternIndex + 1]);
+    }
+    else if ((argc - patternIndex) == 1)
+    {
+        string pattern(argv[patternIndex]);
+        string path(".");
+    }
+    else
+    {
+        displayHelpMessage();
+    }
 }
